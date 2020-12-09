e18e027e07e89c5f1c51a9b2c981079c9c336089
lipemon1 <ramon_felipe@outlook.com>
2020-12-09 01:35:04 -0300

    Visual health bar added to enemies
    Just a visual health bar added to our enemies. Not the health changer

---
+++ Assets/SampleTowerDefence/Scripts/View/Enemy/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SampleTowerDefence.Scripts.View.Enemy
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Slider healthSlider;
+
+        [SerializeField] private Vector3 wantedRotation;
+        
+        private void LateUpdate()
+        {
+            transform.rotation = Quaternion.Euler(wantedRotation);    
+        }
+
+        public void HealthChanged(float newValue)
+        {
+            healthSlider.value = newValue;
+        }
+    }
+}
